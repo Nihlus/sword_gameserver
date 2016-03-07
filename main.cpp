01e2eb157e3b175f16896bb888642d39dee9d6ac
20k <the_berronator@hotmail.co.uk>
2016-03-07 08:43:20 +0000

    test

--- main.cpp
+++ main.cpp
@@ -131,7 +131,7 @@ int main(int argc, char* argv[])
                 printf("found master server\n");
             }
 
-            continue;
+            //continue;
         }
 
         bool any_read = true;
